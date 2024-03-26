f9580727d160805c8200d90f6b4266a9e1c2e11f
Cody Botte <cbotte21@gmail.com>
2024-03-26 19:23:44 -0400

    added activity

--- internal/handler/websocket.go
+++ internal/handler/websocket.go
@@ -10,7 +10,15 @@ import (
 	"github.com/gorilla/websocket"
 	"hive-external-go/schema"
 	"net/http"
+	"time"
 )
+
+/*
+FIRST MESSAGE SEND JWT
+REST OF MESSAGES SEND STATUS UPDATE
+*/
+
+const POLL_TIME_SECONDS = time.Second * 20
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -26,11 +34,12 @@ func gracefullyDisconnect(userClient *datastore.RedisClient[schema.ActiveUser], 
 	}
 }
 
-func handleKicks(userClient *datastore.RedisClient[schema.ActiveUser], kicked *bool, _id string) {
+func handleKicks(conn *websocket.Conn, userClient *datastore.RedisClient[schema.ActiveUser], kicked *bool, _id string) {
 	sub := userClient.Subscribe("kicks")
 	ch := sub.Channel()
 	for msg := range ch {
 		if msg.Payload == _id {
+			conn.WriteMessage(0, []byte("Kicked"))
 			*kicked = true
 			break
 		}
@@ -52,12 +61,39 @@ func integrity(judicialClient *judicial.JudicialServiceClient, _id string) error
 	return nil
 }
 
+func authenticate(user *schema.ActiveUser, jwtParser *jwtParser.JwtParser, judicialClient *judicial.JudicialServiceClient, conn *websocket.Conn, userClient *datastore.RedisClient[schema.ActiveUser], p []byte, kicked *bool) bool {
+	// Decode jwt
+	res, err := jwtParser.Redeem(string(p))
+
+	user.Id, _ = res.GetSubject()
+	user.Role = 1 // TODO: Set based on claim role
+
+	// Check player integrity
+	err = integrity(judicialClient, user.Id)
+	if err != nil {
+		conn.WriteMessage(0, []byte(err.Error()))
+		return false
+	}
+
+	// Login
+	err = login(userClient, *user)
+	if err != nil {
+		conn.WriteMessage(0, []byte(err.Error()))
+		return false
+	}
+	conn.WriteMessage(0, []byte("Connection established."))
+	go handleKicks(conn, userClient, kicked, user.Id)
+
+	return true
+}
+
 func Websocket(w http.ResponseWriter, r *http.Request, userClient *datastore.RedisClient[schema.ActiveUser], judicialClient *judicial.JudicialServiceClient, jwtParser *jwtParser.JwtParser) {
 	// Session variables
 	kicked := false
 	user := schema.ActiveUser{
-		Id:   "",
-		Role: 0,
+		Id:       "",
+		Role:     0,
+		Activity: "/auth/signin",
 	}
 
 	// Configure socket
@@ -76,32 +112,18 @@ func Websocket(w http.ResponseWriter, r *http.Request, userClient *datastore.Red
 			break
 		}
 
-		// Decode jwt
-		res, err := jwtParser.Redeem(string(p))
-
-		user.Id, _ = res.GetSubject()
-		user.Role = 0 // TODO: Set based on claim role
-
-		// Check player integrity
-		err = integrity(judicialClient, user.Id)
-		if err != nil {
-			conn.WriteMessage(0, []byte(err.Error()))
+		if kicked {
 			break
 		}
 
-		// Login
-		err = login(userClient, user)
-		if err != nil {
-			conn.WriteMessage(0, []byte(err.Error()))
-			break
+		if user.Id == "" { // Authenticate
+			if !authenticate(&user, jwtParser, judicialClient, conn, userClient, p, &kicked) {
+				break
+			}
+		} else { // Update status
+			user.Activity = string(p)
+			_ = userClient.Update(user, user)
 		}
-
-		//Handle kicks
-		go handleKicks(userClient, &kicked, user.Id)
-		for !kicked && err == nil {
-			_, _, err = conn.NextReader()
-		}
-		break
 	}
 }
 
--- schema/active_user.go
+++ schema/active_user.go
@@ -4,8 +4,9 @@ import "encoding/json"
 
 // ActiveUser struct
 type ActiveUser struct { //Payload
-	Id   string `bson:"_id,omitempty" json:"_id,omitempty" redis:"_id"`
-	Role int    `bson:"role,omitempty" json:"role,omitempty" redis:"role"`
+	Id       string `bson:"_id,omitempty" json:"_id,omitempty" redis:"_id"`
+	Role     int    `bson:"role,omitempty" json:"role,omitempty" redis:"role"`
+	Activity string `bson:"activity,omitempty" json:"activity,omitempty" redis:"activity"`
 }
 
 func (user ActiveUser) Database() string {
